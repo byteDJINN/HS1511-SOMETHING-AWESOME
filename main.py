27087d5ca3c5d0c73dced46aa781a0cc4e34988e
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-07 20:56:28 +0800

    Fixed Formatting Issues On showBoard()

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # FUNCTIONS
 def setVars():
   # Create dictionary of pieces.
-  global pieces = {}
+  global pieces
+  pieces = {'white': {}, 'black': {}}
   pieces['white']['king'] = '♔'
   pieces['white']['queen'] = '♕'
   pieces['white']['rook'] = '♖'
@@ -17,16 +18,57 @@ def setVars():
   pieces['black']['bishop'] = '♝'
   pieces['black']['knight'] = '♞'
   pieces['black']['pawn'] = '♟'
+
+  # Create shorthand
+  w1 = pieces['white']['king']
+  w2 = pieces['white']['queen']
+  w3 = pieces['white']['rook']
+  w4 = pieces['white']['bishop']
+  w5 = pieces['white']['knight']
+  w6 = pieces['white']['pawn']
+  
+  b1 = pieces['black']['king']
+  b2 = pieces['black']['queen']
+  b3 = pieces['black']['rook']
+  b4 = pieces['black']['bishop']
+  b5 = pieces['black']['knight']
+  b6 = pieces['black']['pawn']
   
   # Create the board
-  global board = [['.' for x in range(8)] for y in range(8)]
+  global board
+  board = [['\u2001' for x in range(8)] for y in range(8)]
+  
+  # Put on white pieces
+  y = 0
+  for piece in [w3, w5, w4, w2, w1, w4, w5, w3]: # Non-Pawns
+    board[0][y] = piece
+    y += 1
+  for y in range(8): # Pawns
+    board[1][y] = w6
+    
+  # Put on black pieces
+  y = 0
+  for piece in [b3, b5, b4, b2, b1, b4, b5, b3]: # Non-Pawns
+    board[7][y] = piece
+    y += 1
+  for y in range(8): # Pawns
+    board[6][y] = b6
+  
 
 def showBoard():
   global board
   for x in range(8):
+    print()
+    print(str(x+1),'|',end='')
     for y in range(8):
-      print(str(x),"|",board[x][y])
-  print(" | abcdefgh")
+      print(board[x][y],end='')
+  print()
+  print("  |",end="")
+  for c in 'abcdefgh':
+    print(c+"\u200A",end="") # NOTE: Close but not 100% accurate
+  print()
   
 
 # MAIN
+setVars()
+showBoard()
