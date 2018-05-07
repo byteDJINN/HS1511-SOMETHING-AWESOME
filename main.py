d7ece3c51c6fae85892b9109f089bedf9c6f85b7
Djimon Jayasundera <woofless77@gmail.com>
2018-05-07 19:00:01 +0800

    Create board

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 # IMPORTS
 
 # FUNCTIONS
+def setVars():
+  # Create dictionary of pieces.
+  global pieces = {}
+  pieces['white']['king'] = '♔'
+  pieces['white']['queen'] = '♕'
+  pieces['white']['rook'] = '♖'
+  pieces['white']['bishop'] = '♗'
+  pieces['white']['knight'] = '♘'
+  pieces['white']['pawn'] = '♙'
+  
+  pieces['black']['king'] = '♚'
+  pieces['black']['queen'] = '♛'
+  pieces['black']['rook'] = '♜'
+  pieces['black']['bishop'] = '♝'
+  pieces['black']['knight'] = '♞'
+  pieces['black']['pawn'] = '♟'
+  
+  # Create the board
+  global board = [['' for x in range(8)] for y in range(8)]
+  
 
 # MAIN
