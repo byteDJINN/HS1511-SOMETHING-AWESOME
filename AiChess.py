e59e0b438dabb8f1e00fe95afd7273980f48319a
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-10 19:13:25 +0800

    Create AiChess.py

---
+++ AiChess.py
@@ -0,0 +1,14 @@
+# IMPORTS
+import userChess.py
+
+# FUNCTIONS
+
+def getRating():
+  # Produces a rating of the position of a board
+  
+def decideMove():
+  # Blah Blah Blah
+  # Decide what to do...
+  
+  movePiece(piece, start, finish) # Make the move
+  
