5a78dbee4894b310db936a984e029ec504e09267
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-10 19:17:37 +0800

    Added pseudocode

--- runChess.py
+++ runChess.py
@@ -1,1 +1,17 @@
 
+''' PSUEDO CODE
+import ai1, ai2
+import userChess
+
+while (game not over):
+  if getTurnNumber() % 2 == 1: # If it is an odd number
+    ai1.decideMove()
+  else:
+    ai2.decideMove()
+  
+  if (game over):
+    win = whoever won
+
+print(win, "WON!!!")
+
+'''
