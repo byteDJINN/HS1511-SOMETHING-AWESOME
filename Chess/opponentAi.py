4e32191ffc6155585faba6bcd9afa87a7b7ed157
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-12 20:21:45 +0800

    Create opponentAi.py

---
+++ Chess/opponentAi.py
@@ -0,0 +1,8 @@
+import userChess as u
+
+def decideMove():
+  board = u.getBoard()
+  start = list(input("Start: ")) # In format "x y"
+  finish = list(input("Finish: ")) # In format "x y"
+  u.movePiece(board, start, finish)
+
