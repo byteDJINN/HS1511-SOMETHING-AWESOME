d342df7148e09ba9209157d230fec448cde0d267
Djimon Jayasundera <woofless77@gmail.com>
2018-05-13 14:16:47 +0800

    Delete player.py

--- Chess/player.py
+++
@@ -1,8 +0,0 @@
-import userChess as u
-
-def decideMove():
-  board = u.getBoard()
-  start = list(input("Start: ")) # In format "x y"
-  finish = list(input("Finish: ")) # In format "x y"
-  return u.movePiece(board, start, finish)
-
