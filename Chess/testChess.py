11c62dc1b9673fdfcc74b2cf362b9908386811a1
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-15 21:48:32 +0800

    Made it print each lien

--- Chess/testChess.py
+++ Chess/testChess.py
@@ -4,12 +4,17 @@ print("Checking setVars()")
 c.setVars()
 
 print("Checking isGameOver(board)")
-assert(c.isGameOver(board) == False)
+if not (c.isGameOver(board) == False):
+  print("isGameOver failed.")
 
 print("Checking isValidPos(board, start, finish)")
-assert(c.isValidPos([8, 8]) == False) # Fence-post error check
-assert(c.isValidPos([100298, 301847]) == False) # Normal error check
+if not (c.isValidPos([8, 8]) == False): # Fence-post error check
+  print("isValidPos failed.")
+if not (c.isValidPos([100298, 301847]) == False): # Normal error check
+  print("isValidPos failed.")
 
 print("Checking isValidMove(board, piece, start, finish)")
-assert(c.isValidMove(board, c.w6, [0, 7], [0, 6]) == True) # 1 Square move (pawn)
-assert(c.isValidMove(board, c.w6, [0, 7], [0, 5]) == True) # 2 Square move (pawn)
+if not (c.isValidMove(board, c.w6, [0, 7], [0, 6]) == True): # 1 Square move (pawn)
+  print("isValidMove failed.")
+if not (c.isValidMove(board, c.w6, [0, 7], [0, 5]) == True): # 2 Square move (pawn)
+  print("isValidMove failed.")
