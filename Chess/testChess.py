00038529c4beaf37e9c3f28db8379056333504bc
Djimon Jayasundera <djimondjayasundera@icloud.com>
2018-05-16 12:44:11 +0800

    Made it use functions to reduce replication

--- Chess/testChess.py
+++ Chess/testChess.py
@@ -1,22 +1,27 @@
 import chess as c # This file is just for testing so it is allowed to violate abstraction
 
+def printCheck(status, check): # Status is True or False, Check is the test number
+  if status:
+    print("Test " + str(check) + ": succeeded.")
+  else:
+    print("Test " + str(check) + ": failed.")
+
 print("Checking setVars()")
 c.setVars()
+printCheck(c.pieces["white"]["king"] == c.w1, 1)
 
-print("Checking isGameOver(board)")
-if not (c.isGameOver(board) == False):
-  print("isGameOver failed.")
+print("\nChecking isGameOver(board)")
+printCheck(not c.isGameOver(c.board), 1)
 
-print("Checking isValidPos(board, start, finish)")
-if not (c.isValidPos([8, 8]) == False): # Fence-post error check
-  print("isValidPos failed.")
-if not (c.isValidPos([100298, 301847]) == False): # Normal error check
-  print("isValidPos failed.")
+print("\nChecking isValidPos(board, start, finish)")
+printCheck(not c.isValidPos([8, 8]), 1) # Fence-post error check
+printCheck(not c.isValidPos([100298, 301847]), 2) # Normal error check
 
-print("Checking isValidMove(board, piece, start, finish)")
-if not (c.isValidMove(board, c.w6, [0, 7], [0, 6]) == True): # 1 Square move (pawn)
-  print("isValidMove failed.")
-if not (c.isValidMove(board, c.w6, [0, 7], [0, 5]) == True): # 2 Square move (pawn)
-  print("isValidMove failed.")
+print("\nChecking isValidMove(board, piece, start, finish)")
+printCheck(c.isValidMove(c.board, c.w6, [0, 7], [0, 6]), 1) # 1 Square move (pawn)
+printCheck(c.isValidMove(c.board, c.w6, [0, 7], [0, 5]), 2) # 2 Square move (pawn)
 
-print("All tests passed. You are AWESOME!!!")
+print("\nChecking isObstruction(start, finish)")
+printCheck(c.isObstruction([0, 6], [0, 5]), 1)
+
+print("\nTesting Complete.")
